39c27361e6ee5b8c39ec1287791208e2d6d45fe3
Eric Lee <szl0094@auburn.edu>
2022-10-10 14:46:11 -0500

    constructing capsule component

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -1,34 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
-ABasePawn::ABasePawn()
-{
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+ABasePawn::ABasePawn() {
+    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsul Collider"));
 }
 
 // Called when the game starts or when spawned
-void ABasePawn::BeginPlay()
-{
-	Super::BeginPlay();
-	
+void ABasePawn::BeginPlay() {
+    Super::BeginPlay();
 }
 
 // Called every frame
-void ABasePawn::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+void ABasePawn::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
-void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
+void ABasePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
-
