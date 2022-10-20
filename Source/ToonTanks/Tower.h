4bbf7b49244e159c9b3239b8142c7683415a5f00
Eric Lee <szl0094@auburn.edu>
2022-10-20 15:09:56 -0500

    Tower now turns if player tank is in range

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn {
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	class ATank* Tank;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float FireRange = 600.f;
+};
