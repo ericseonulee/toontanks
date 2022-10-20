bc5a295dc2da938522fafd49b30298dc4aafc502
Eric Lee <szl0094@auburn.edu>
2022-10-20 14:54:30 -0500

    Indent

--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -13,33 +13,33 @@ UCLASS()
 class TOONTANKS_API ATank : public ABasePawn {
 	GENERATED_BODY()
 
-	public:
-		ATank();
+public:
+	ATank();
 
-		// Called to bind functionality to input
-		virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-		// Called every frame
-		virtual void Tick(float DeltaTime) override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
-	protected:
-		// Called when the game starts or when spawned
-		virtual void BeginPlay() override;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
-	private:
-		UPROPERTY(VisibleAnywhere, Category = "Components")
-		class USpringArmComponent* SpringArm;
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArm;
 
-		UPROPERTY(VisibleAnywhere, Category = "Components")
-		class UCameraComponent* Camera;
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* Camera;
 
-		void Move(float Value);
-		void Turn(float Value);
+	void Move(float Value);
+	void Turn(float Value);
 
-		APlayerController* PlayerControllerRef;
+	APlayerController* PlayerControllerRef;
 
-		UPROPERTY(EditAnywhere, Category = "Movement");
-		float Speed = 500.0f;
-		UPROPERTY(EditAnywhere, Category = "Movement")
-		float TurnRate = 90.0f;
+	UPROPERTY(EditAnywhere, Category = "Movement");
+	float Speed = 500.0f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float TurnRate = 90.0f;
 };
