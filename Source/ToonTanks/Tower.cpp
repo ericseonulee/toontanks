87afacee28024df929b6c6d0614dbf737598b47f
Eric Lee <szl0094@auburn.edu>
2022-10-21 14:31:57 -0500

    Towers fire with timer

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -9,16 +9,31 @@ void ATower::BeginPlay() {
     Super::BeginPlay();
 
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+    GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
 }
 
 void ATower::Tick(float DeltaTime) {
     Super::Tick(DeltaTime);
 
+    if (InFireRange()) {
+        RotateTurret(Tank->GetActorLocation());
+    }
+
+}
+
+void ATower::CheckFireCondition() {
+    if (InFireRange()) {
+        Fire();
+    }
+}
+
+bool ATower::InFireRange() {
     if (Tank) {
         float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
 
-        if (Distance <= FireRange) {
-            RotateTurret(Tank->GetActorLocation());
-        }
+        return Distance <= FireRange;
     }
+    
+    return false;
 }
--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -27,4 +27,9 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 600.f;
+
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+	bool InFireRange();
 };
