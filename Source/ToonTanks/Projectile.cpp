50e5bac5385af3fa68aba98edfb1c162bad34317
Eric Lee <szl0094@auburn.edu>
2023-01-11 16:33:48 -0600

    added widget to display text on canvas

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -43,6 +43,6 @@ void AProjectile::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor, UPrimi
 
 	if (OtherActor && OtherActor != this && OtherActor != MyOwner) {
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
-		Destroy();
 	}
+		Destroy();
 }
--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -21,6 +21,9 @@ protected:
 
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGame();
+
 private:
 
 	class ATank* Tank;
